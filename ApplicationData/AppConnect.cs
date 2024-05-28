e62051bb74779c181f1189062ecc77e9084d8b5b
u31510 <u31510@P31510>
2024-05-29 09:39:57 +1000

    добавила окна пользователей, задала минимальный размер окна, добавила ERD, подключение к БД,

---
+++ ApplicationData/AppConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceKuchura_12.ApplicationData
+{
+    internal class AppConnect
+    {
+        /// <c>
+        /// метод подключения к БД
+        /// </c>
+        public static DBCarServiceKuchura12Entities dBCar;
+    }
+}
