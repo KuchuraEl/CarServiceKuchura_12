c57f950b0944f51b2c94a293e44ec5e04f986cff
u31510 <u31510@P31510>
2024-05-29 10:10:03 +1000

    окно авторизации

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CarServiceKuchura_12.ApplicationData;
+using CarServiceKuchura_12.Windows;
 
 namespace CarServiceKuchura_12
 {
@@ -23,6 +25,66 @@ namespace CarServiceKuchura_12
         public MainWindow()
         {
             InitializeComponent();
+            /// <c>
+            /// Подключение БД
+            /// </c>
+            AppConnect.dBCar = new ApplicationData.DBCarServiceKuchura12Entities();
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            /// <summary>
+            /// код для авторизации: поиск по логину/паролю и присвоение роли
+            /// </summary>
+            try
+            {
+                user curUser = DBCarServiceKuchura12Entities.GetContext().user.FirstOrDefault(p => p.login == txbLogin.Text && p.password == pxbPassword.Password);
+
+                if (curUser == null)
+                    MessageBox.Show("Пользователь не найден");
+                else
+                {
+                    winCarDiagnost winCarDiagnost = new winCarDiagnost();
+                    winCarMechanic winCarMechanic = new winCarMechanic();
+                    winMasterReceipt winMasterReceipt = new winMasterReceipt();
+
+                    winGuest winGuest = new winGuest();
+
+                    switch (curUser.userroleid)
+                    {
+                        case 1:
+                            MessageBox.Show("Здравствуйте, автомеханик " + curUser.firstname);
+                            var MW = Window.GetWindow(this);
+                            winCarMechanic.Show();
+                            MW.Close();
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте, мастер приемщик " + curUser.firstname);
+                            MW = Window.GetWindow(this);
+                            winMasterReceipt.Show();
+                            MW.Close();
+                            break;
+                        case 3:
+                            MessageBox.Show("Здравствуйте, автодиагност " + curUser.firstname);
+                            MW = Window.GetWindow(this);
+                            winCarDiagnost.Show();
+                            MW.Close();
+                            break;
+                    }
+                }
+            }
+            /// <summary>
+            /// обработка исключения
+            /// </summary>
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnGuest_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
